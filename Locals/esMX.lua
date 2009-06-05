6ba97b8d5efaf627814bc8aaea3257400172ec9f
ackis <ackis@9af1ac6e-b324-48a7-bcec-c81ff0b4e86c>
2009-06-05 20:55:38 +0000

    Convert most locales to the new localization app, add esMX and koKR so they can get auto-popped.

--- Locals/esMX.lua
+++ Locals/esMX.lua
@@ -1,8 +1,8 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale( "Altoholic", "esES" )
+﻿local L = LibStub("AceLocale-3.0"):NewLocale( "Altoholic", "esMX" )
 
 if not L then return end
 
---@localization(locale="esES", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=false, same-key-is-true=true)@
+--@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=false, same-key-is-true=true)@
 
 if GetLocale() == "esES" then
 -- Altoholic.xml local
