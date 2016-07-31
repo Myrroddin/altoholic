19aa2454acef0cedc11ad8209cf6e3d44b97fb99
thaoky <thaoky@9af1ac6e-b324-48a7-bcec-c81ff0b4e86c>
2016-07-31 15:40:46 +0000

    7.0.005 Commit

--- trunk/Frames/TabOptions.lua
+++ trunk/Frames/TabOptions.lua
@@ -120,6 +120,11 @@ local support = {
 
 -- this content will be subject to frequent changes, do not bother translating it !!
 local whatsnew = {
+	{	name = "7.0.005 Changes",
+		bulletedList = {
+			"DataStore: Fixed a lua error when getting AutoCompleteRealms.",
+		},
+	},
 	{	name = "7.0.004 Changes",
 		bulletedList = {
 			"Fixed issues related to connected realms (guild bank, tooltips, etc..)",
