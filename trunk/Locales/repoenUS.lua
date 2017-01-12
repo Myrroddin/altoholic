b30c49b52a34a5134a50024161d5ff14d9625d50
thaoky.altoholic@yahoo.com <thaoky.altoholic@yahoo.com@9af1ac6e-b324-48a7-bcec-c81ff0b4e86c>
2017-01-12 19:36:09 +0000

    7.1.004 Commit

--- trunk/Locales/repoenUS.lua
+++ trunk/Locales/repoenUS.lua
@@ -171,6 +171,8 @@ L["TALENT_SPECIALIZATION_STAT_PRIORITY"] = "Stat priority"
 -- *** Tab Achievements 
 -- *************************
 
+L["ACCOUNT_WIDE"] = "Account Wide"
+
 -- *************************
 -- *** Tab Agenda 
 -- *************************
@@ -485,15 +487,9 @@ L["up to"] = true
 L["at"] = true
 L["and above"] = true
 L["Suggestion"] = true
-L["Prof. 1"] = true
-L["Prof. 2"] = true
 L["Grey"] = true
 L["All cooldowns are up"] = true
 
--- talents.lua
-L["Compare with"] = true
-L["Other guilds"] = true
-L["Other characters"] = true
 
 -- TabSummary.lua
 L["All accounts"] = true
@@ -932,9 +928,7 @@ L["Send account sharing request to:"] = true
 L["Memory used"] = true
 
 -- ** Frame 1 : General **
-L["Max rest XP displayed as 150%"] = true
-L["Show FuBar icon"] = true
-L["Show FuBar text"] = true
+
 L["Account Sharing Enabled"] = true
 L["Guild Communication Enabled"] = true
 
